0b147f0014c32a380b62d5b2950cd19662cf1a53
James Downey <james.m.downey30@gmail.com>
2025-04-08 14:23:01 -0600

    little things

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { print, visitUrl } from "kolmafia";
+
 import * as minis from "./minis";
 
+/**
+ * Stores data from the active PVP minigames.
+ */
 interface minigame {
   name: string;
   description: string;
@@ -10,17 +14,20 @@ interface minigame {
 
 const minisRegex = /<tr class="small">(.*?)<\/tr>/g;
 const miniNameRegex = /<td valign="top" nowrap><b>(.*?)<\/b>/;
-const miniDescRegex = /<td valign="top">(.*?)<\/td>/
-const miniScoreRegex = /<td valign="top" align="center" colspan="1>(.*?)<\/td>/
-const miniScoreHCRegex = /<td valign="top" align="center">(.*?)<\/td>/
-
-let minimizeEquipPower = false
+const miniDescRegex = /<td valign="top">(.*?)<\/td>/;
+const miniScoreRegex = /<td valign="top" align="center" colspan="1>(.*?)<\/td>/;
+const miniScoreHCRegex = /<td valign="top" align="center">(.*?)<\/td>/;
 
 export default function main(): void {
+  // Let's hit the pvp rules page and generate our minigames.
+  // Maybe in the future we could store the date/data and only generate these when they're out of date? Not sure how safe that is though.
   const minisPage = visitUrl(`/peevpee.php?place=rules`);
-  const minisArrayRaw = Array.from(minisPage.matchAll(minisRegex));
 
-  const minisArray: minigame[] = minisArrayRaw.map((miniString) => {
+  // Match the lines for the specific minigames.
+  const miniArrayRaw = Array.from(minisPage.matchAll(minisRegex));
+
+  // Match the names, descriptions, and scores for each minigame and store them nicely in an array.
+  const miniArray: minigame[] = miniArrayRaw.map((miniString) => {
     return {
       name: miniString[1].match(miniNameRegex)?.[1] ?? "",
       description: miniString[1].match(miniDescRegex)?.[1] ?? "",
@@ -29,11 +36,100 @@ export default function main(): void {
     };
   });
 
-  if (minisArray.some(mini => minis.lightestLoadMini.includes(mini.name))) {
-    minimizeEquipPower = true
-  }
+  // Maybe later we'll print each minigame to the CLI all pretty-like.
+  // miniArray.forEach(mini => print(`${mini.name} - ${mini.description} - ${mini.score} - ${mini.scoreHC}`))
 
-  if (minimizeEquipPower)
-    print("I love you")
+  // Now that we've got out minigames, we can match them against our minis.ts to see what we've got maybe?
+  // I'm not sure what the best way to optimize this stuff lmao but a bunch of if statements that flip flags
+  // we use to do the optimization calculations at the end feels.... like not the best option.
 
+  miniArray.forEach((mini) => {
+    if (minis.verbosity(mini.name)) {
+      print("verbosity");
+    }
+    if (minis.laconic(mini.name)) {
+      print("laconic");
+    }
+    if (minis.showingInitiative(mini.name)) {
+      print("showingInitiative");
+    }
+    if (minis.peaceOnEarth(mini.name)) {
+      print("peaceOnEarth");
+    }
+    if (minis.eggHunt(mini.name)) {
+      print("eggHunt");
+    }
+    if (minis.meatLover(mini.name)) {
+      print("meatLover");
+    }
+    if (minis.weaponDamage(mini.name)) {
+      print("weaponDamage");
+    }
+    if (minis.boozeDrop(mini.name)) {
+      print("boozeDrop");
+    }
+    if (minis.foodDrop(mini.name)) {
+      print("foodDrop");
+    }
+    if (minis.allResistance(mini.name)) {
+      print("allResistance");
+    }
+    if (minis.coldResistance(mini.name)) {
+      print("coldResistance");
+    }
+    // if (minis.hotResistance(mini.name)) {
+    //   print("hotResistance");
+    // }
+    // if (minis.sleazeResistance(mini.name)) {
+    //   print("sleazeResistance");
+    // }
+    if (minis.stenchResistance(mini.name)) {
+      print("stenchResistance");
+    }
+    // if (minis.spookyResistance(mini.name)) {
+    //   print("spookyResistance");
+    // }
+    // if (minis.coldDamage(mini.name)) {
+    //   print("coldDamage");
+    // }
+    if (minis.hotDamage(mini.name)) {
+      print("hotDamage");
+    }
+    if (minis.sleazeDamage(mini.name)) {
+      print("sleazeDamage");
+    }
+    // if (minis.stenchDamage(mini.name)) {
+    //   print("stenchDamage");
+    // }
+    // if (minis.spookyDamage(mini.name)) {
+    //   print("spookyDamage");
+    // }
+    if (minis.leastPower(mini.name)) {
+      print("leastPower");
+    }
+    if (minis.mostPower(mini.name)) {
+      print("mostPower");
+    }
+    if (minis.barelyDressed(mini.name)) {
+      print("barelyDressed");
+    }
+    if (minis.noel(mini.name)) {
+      print("noel");
+    }
+    if (minis.letterOfTheMoment(mini.name)) {
+      print("letterOfTheMoment");
+    }
+    if (minis.deface(mini.name)) {
+      print("deface");
+    }
+    if (minis.nines(mini.name)) {
+      print("nines");
+    }
+    if (minis.beastMaster(mini.name)) {
+      print("beastMaster");
+    }
+    if (minis.safariChic(mini.name)) {
+      print("safariChic");
+    }
+  });
 }
--- src/minis.ts
+++ src/minis.ts
@@ -1,5 +1,132 @@
-export const lightestLoadMini = [
-    "Lightest Load",
-    "Optimal Dresser",
-    "Wardrobe Malfunction",
-]
+/**
+ * This minigame counts the total length (in "characters") of the names of all equipment worn (including familiar equipment). The largest total wins.
+ */
+export function verbosity(mini: string): boolean {
+  return ["Verbosity Demonstration", "It's a Mystery, Also"].includes(mini);
+}
+
+/**
+ * This minigame counts the total length (in "characters") of the names of all equipment worn. The lowest total wins. An empty equipment slot counts as 23 points.
+ */
+export function laconic(mini: string): boolean {
+  return ["Laconic Dresser", "Dressed in Rrrags", "Outfit Compression"].includes(mini);
+}
+
+export function showingInitiative(mini: string): boolean {
+  return ["Showing Initiative", "Early Shopper", "Motivated by Irony"].includes(mini);
+}
+
+export function peaceOnEarth(mini: string): boolean {
+  return ["Peace on Earth", "Sooooper Sneaky", "Smellin' Like a Stinkin' Rose"].includes(mini);
+}
+
+export function eggHunt(mini: string): boolean {
+  return ["The Egg Hunt", "The Optimal Stat"].includes(mini);
+}
+
+export function meatLover(mini: string): boolean {
+  return ["Meat Lover"].includes(mini);
+}
+
+export function weaponDamage(mini: string): boolean {
+  return ["Maul Power"].includes(mini);
+}
+
+export function boozeDrop(mini: string): boolean {
+  return [
+    "Moarrrrrr Booze!",
+    "A Nice Cold One",
+    "Holiday Spirit(s)!",
+    "Thirrrsty forrr Booze",
+  ].includes(mini);
+}
+
+export function foodDrop(mini: string): boolean {
+  return ["New Tastes"].includes(mini);
+}
+
+export function allResistance(mini: string): boolean {
+  return ["Broad Resistance Contest"].includes(mini);
+}
+
+export function coldResistance(mini: string): boolean {
+  return ["All Bundled Up", "Hibernation Ready"].includes(mini);
+}
+
+// export function hotResistance(mini: string): boolean {
+//   return [""].includes(mini);
+// }
+
+// export function sleazeResistance(mini: string): boolean {
+//   return [""].includes(mini);
+// }
+
+export function stenchResistance(mini: string): boolean {
+  return ["Hold Your Nose"].includes(mini);
+}
+
+// export function spookyResistance(mini: string): boolean {
+//   return [""].includes(mini);
+// }
+
+// export function coldDamage(mini: string): boolean {
+//   return [""].includes(mini);
+// }
+
+export function hotDamage(mini: string): boolean {
+  return ["Ready to Melt", "Fahrenheit 451", "Hot for Teacher"].includes(mini);
+}
+
+export function sleazeDamage(mini: string): boolean {
+  return ["Innuendo Master"].includes(mini);
+}
+
+// export function stenchDamage(mini: string): boolean {
+//   return [""].includes(mini);
+// }
+
+// export function spookyDamage(mini: string): boolean {
+//   return [""].includes(mini);
+// }
+
+/**
+ * This minigame compares the total power of each player's hat, shirt and pants (lowest total wins).
+ */
+export function leastPower(mini: string): boolean {
+  return ["Lightest Load", "Optimal Dresser", "Wardrobe Malfunction"].includes(mini);
+}
+
+/**
+ * This minigame compares the total power of each player's hat, shirt and pants (highest total wins, ignores Tao of the Terrapin).
+ */
+export function mostPower(mini: string): boolean {
+  return ["Fashion Show", "School Pride"].includes(mini);
+}
+
+export function barelyDressed(mini: string): boolean {
+  return ["Barely Dressed"].includes(mini);
+}
+
+export function noel(mini: string): boolean {
+  return ["Spirit of Noel"].includes(mini);
+}
+
+export function letterOfTheMoment(mini: string): boolean {
+  return ["Letter of the Moment", "ASCII-7 of the moment", "Spirit Day"].includes(mini);
+}
+
+export function deface(mini: string): boolean {
+  return ["DEFACE"].includes(mini);
+}
+
+export function nines(mini: string): boolean {
+  return ["Dressed to the 9s"].includes(mini);
+}
+
+export function beastMaster(mini: string): boolean {
+  return ["Beast Master"].includes(mini);
+}
+
+export function safariChic(mini: string): boolean {
+  return ["Safarai Chic"].includes(mini);
+}
